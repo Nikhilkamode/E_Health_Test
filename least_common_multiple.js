994413eb5b8a4c69918e36d3c7fc0888324839b9
Nikhil Kamode <112844869+Nikhilkamode@users.noreply.github.com>
2024-06-02 13:24:23 +0530

    least_common_multiple.js

---
+++ least_common_multiple.js
@@ -0,0 +1,27 @@
+//sample Usage lcm_multiple([1,3,4,5,6])
+
+function gcd(a, b){
+    var t;
+    while (b != 0){
+        t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+function lcm(a, b){
+    return (a * b / gcd(a, b));
+}
+
+function lcm_multiple(args){
+    // The args variable will contain all the arguments passed in the arrayRecursively iterate through pairs of arguments
+
+    if(args.length == 2){
+        return lcm(args[0], args[1]);
+    } else {
+        var arg0 = args[0];
+        args.shift();
+        return lcm(arg0, lcm_multiple(args));
+    }
+}
